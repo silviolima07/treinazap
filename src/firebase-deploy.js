fa2fe90bc098d18f0d9c6e87a341b977597494db
silviolima07 <silviolima07@gmail.com>
2021-03-04 17:20:34 -0300

    Update firebase-deploy.js

--- src/firebase-deploy.js
+++ src/firebase-deploy.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-   apiKey: "{{VAR_API}",
-    authDomain: "{{VAR_AUTH}",
-    projectId: "{{VAR_PROJECT}",
-    storageBucket: "{{VAR_STORAGE}",
-    messagingSenderId: "{{VAR_MESS}",
-    appId: "{{VAR_APP}"
+   apiKey: "{VAR_API}",
+    authDomain: "{VAR_AUTH}",
+    projectId: "{VAR_PROJECT}",
+    storageBucket: "{VAR_STORAGE}",
+    messagingSenderId: "{VAR_MESS}",
+    appId: "{VAR_APP}"
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
